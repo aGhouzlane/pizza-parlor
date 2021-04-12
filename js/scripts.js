0ff4dff7a4b26f04586465ef78d155940075a30a
aGhouzlane <ahmedghouzlane@gmail.com>
2021-04-12 16:40:41 -0400

    Add Business Logic

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+//Business Logic -------
+
+function CustomerOrder() {
+  this.pizzas = {};
+  this.currentId = 0;
+}
+
+CustomerOrder.prototype.addPizza = function (pizza) {
+  pizza.id = this.assignId();
+  this.pizzas[pizza.id] = pizza;
+}
+
+CustomerOrder.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.deleteContact = function (id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+}
+
+function Pizza(crustSize, crustType, toppings = []) {
+  this.crustSize = crustSize;
+  this.crustType = crustType;
+  this.toppings = toppings;
+}
+
+let order = new CustomerOrder();
+let pizza1 = new Pizza("Large", "Thin Crust", ["Cheese", "Peporoni", "Tomato", "Red Onion"]);
+let pizza1 = new Pizza("X-Large", "Regular Crust", ["Cheese", "Peporoni", "Tomato", "Red Onion"]);
+
+order.addPizza(pizza1);
+order.addPizza(pizza2);
+
+console.log(order);
+
+
+
+
+
+
+//User Interface Logic -------
