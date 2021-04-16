ee08b5e03aa8bb33f82f0fdda75ae454c539bad0
aGhouzlane <ahmedghouzlane@gmail.com>
2021-04-15 21:56:05 -0400

    Add input logic for selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 //Business Logic -------
-
 function CustomerOrder() {
   this.pizzas = {};
   this.currentId = 0;
@@ -15,7 +14,15 @@ CustomerOrder.prototype.assignId = function () {
   return this.currentId;
 }
 
-CustomerOrder.prototype.deleteContact = function (id) {
+CustomerOrder.prototype.findPizza = function (id) {
+  if (this.pizzas[id] != undefined) {
+    return this.pizzas[id];
+  }
+  return false;
+}
+
+
+CustomerOrder.prototype.deletePizza = function (id) {
   if (this.pizzas[id] === undefined) {
     return false;
   }
@@ -29,18 +36,85 @@ function Pizza(crustSize, crustType, toppings = []) {
   this.toppings = toppings;
 }
 
-let order = new CustomerOrder();
-let pizza1 = new Pizza("Large", "Thin Crust", ["Cheese", "Peporoni", "Tomato", "Red Onion"]);
-let pizza2 = new Pizza("X-Large", "Regular Crust", ["Cheese", "Peporoni", "Tomato", "Red Onion"]);
-
-order.addPizza(pizza1);
-order.addPizza(pizza2);
-
-console.log(order);
-
-
-
-
-
-
 //User Interface Logic -------
+let customerOrder = new CustomerOrder();
+
+function attachPizzaListeners() {
+  $("ul#pizzas").on("click", "li", function () {
+    showPizza(this.id);
+  });
+
+  $("#buttons").on("click", ".deleteButton", function () {
+    customerOrder.deletePizza(this.id);
+    $("#show-pizza").hide();
+    displayPizzaDetails(customerOrder);
+  });
+};
+
+function showPizza(pizzaId) {
+  const pizza = customerOrder.findPizza(pizzaId);
+  $("#show-pizza").show();
+
+  pizza.crustSize = document.getElementById('option').value;
+  $(".crust-type").html(pizza.crustSize);
+
+  if (document.getElementById('new-crust-type').checked) {
+    pizza.crustType = "Thin Crust";
+    $(".curst-size").html(pizza.crustType);
+  }
+
+  $(".cheese").html(pizza.toppings[0]);
+
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + pizza.id + ">Delete</button>");
+}
+
+function displayPizzaDetails(customerOrderDisplay) {
+  let pizzasList = $("ul#pizzas");
+  let htmlForPizzaInfo = "";
+  Object.keys(customerOrderDisplay.pizzas).forEach(function (key) {
+    const pizza = customerOrderDisplay.findPizza(key);
+    htmlForPizzaInfo += "<li id=" + pizza.id + ">" + pizza.crustSize + " " + pizza.crustType + "</li>";
+  });
+  pizzasList.html(htmlForPizzaInfo);
+};
+
+$(document).ready(function () {
+  attachPizzaListeners();
+  $("form#new-pizza").submit(function (event) {
+
+    event.preventDefault();
+    const inputtedCrustSize = $("input#new-crust-size").val();
+    const inputtedCrustType = $("input#new-crust-type").val();
+    const inputtedCheese = $("input#new-cheese").val();
+    const inputtedTomato = $("input#new-tomato").val();
+    const inputtedPepperoni = $("input#new-pepperoni").val();
+    const inputtedRedOnion = $("input#new-redonion").val();
+    const inputtedOliveOil = $("input#new-oliveoil").val();
+    const inputtedBlackOlives = $("input#new-blackolives").val();
+    const inputtedToppings = [
+      inputtedCheese, inputtedTomato,
+      inputtedPepperoni, inputtedRedOnion,
+      inputtedOliveOil, inputtedBlackOlives];
+
+    console.log(inputtedToppings);
+
+    // The next three lines are new:
+    $("input#new-crust-size").val("");
+    $("input#new-crust-type").val("");
+    $("input#new-cheese").val("");
+    $("input#new-tomato").val("");
+    $("input#new-pepperoni").val("");
+    $("input#new-redonion").val("");
+    $("input#new-oliveoil").val("");
+    $("input#new-blackolives").val("");
+
+
+    let newPizza = new Pizza(inputtedCrustSize, inputtedCrustType, inputtedToppings);
+    customerOrder.addPizza(newPizza);
+
+    console.log(customerOrder.pizzas);
+    displayPizzaDetails(customerOrder);  // <--- This is the new line!
+  });
+});
